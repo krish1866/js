48dedd63032cb563a9dadd52030f6341b5e3be5b
krish1866 <kumarkrish151723@gmail.com>
2024-12-02 14:44:11 +0530

    Add files via upload

---
+++ krrish web day 6/index.js
@@ -0,0 +1,38 @@
+console.log("-----welcome in js------");
+
+//variable:var, let,const:
+document.write("Hello world");
+//console.log(a)
+let a=5;
+console.log(b)
+var b=5;
+
+const c=10;
+console.log(c)
+a =9;
+b =5;
+// c =9;
+a ="abc";
+console.log(a);
+if (a===b);
+//  console.log("hello world in if condition")
+// }else {
+// console.log("else condition");
+// }
+
+// loop: for and while:
+// 9 * 1 = 9
+// 9 * 2 = 18
+// 9 * 3 = 27
+// 9 * 4 = 36
+// 9 * 5 = 45
+// 9 * 6 = 54
+// 9 * 7 = 63
+// 9 * 8 = 72
+// 9 * 9 = 81
+// 9 * 10 = 90
+
+for (let i = 1; i < 11; i++){
+    console.log(i*9);
+}
+
